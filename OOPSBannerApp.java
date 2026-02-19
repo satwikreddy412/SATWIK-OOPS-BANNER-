06d70e0a156365ac195a094415b997d900df82bc
satwikreddy412 <satwiksabella0412@gmail.com>
2026-02-19 16:42:30 +0530

    UC1: Print OOPS to console implemented

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print the literal text "OOPS" to the console.
+ *
+ * @author Satwik
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+    }
+}
