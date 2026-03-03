5d23ac6503dde58df700b46bb8b8690423c4d967
satwikreddy412 <satwiksabella0412@gmail.com>
2026-03-03 17:42:08 +0530

    UC4 - Refactored banner using String array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,18 @@
-/**
- * OOPSBannerApp
- *
- * UC1: Print the literal text "OOPS" to the console.
- *
- * @author Satwik
- * @version 1.0
- */
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application
-     */
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        // Creating banner using String array
+        String[] banner = {
+                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"),
+                "*        OOPS BANNER APP        *",
+                "*        UC4 - Arrays & Loops   *",
+                String.join("", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*")
+        };
+
+        // Printing banner using enhanced for-loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
